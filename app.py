b92b7c41e0f4cf4dc6f913d36f0311630d9e5467
Lashmanbala <lashmanbala@gmail.com>
2024-06-17 02:53:19 +0000

    Creating dataframes

--- app.py
+++ app.py
@@ -5,3 +5,23 @@ spark = SparkSession. \
             appName('compute_daily_revenue'). \
             master('yarn'). \
             getOrCreate()
+
+orders = spark. \
+        read. \
+        csv(
+            '/user/hadoop/retail_db/orders',
+            schema='''
+                order_id INT, order_date STRING,
+                order_customer_id INT, order_status STRING
+                '''
+        )
+
+order_items = spark. \
+                read. \
+                csv(
+                    '/user/hadoop/retail_db/order_items',
+                    schema='''
+                            order_item_id INT,order_item_order_id INT, order_item_product_id INT,
+                            order_item_quantity INT,order_item_subtotal FLOAT, order_item_product_price FLOAT
+                            '''
+                )
