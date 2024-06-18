451d47fedc66886713a7bcdeeb2ba68a50ba135f
Lashmanbala <lashmanbala@gmail.com>
2024-06-18 06:45:46 +0000

    Setting up environment variables in cluster mode

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 spark-submit \
     --deploy-mode cluster \
+    --conf spark.yarn.appMasterEnv.SRC_BASE_DIR=/user/`whoami`/retail_db \
+    --conf spark.yarn.appMasterEnv.TGT_BASE_DIR=/user/`whoami`/retail_db/result \
     /home/`whoami`/emr/app.py
