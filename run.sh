c2114295c1ee7ae7c4150a9a35af4786f14443c0
Lashmanbala <lashmanbala@gmail.com>
2024-06-18 06:56:24 +0000

    Downloading delta packages and adding configurations for delta

--- run.sh
+++ run.sh
@@ -2,4 +2,7 @@ spark-submit \
     --deploy-mode cluster \
     --conf spark.yarn.appMasterEnv.SRC_BASE_DIR=/user/`whoami`/retail_db \
     --conf spark.yarn.appMasterEnv.TGT_BASE_DIR=/user/`whoami`/retail_db/result \
+    --packages io.delta:delta-spark_2.12:3.2.0 \
+    --conf "spark.sql.extensions=io.delta.sql.DeltaSparkSessionExtension" \
+    --conf "spark.sql.catalog.spark_catalog=org.apache.spark.sql.delta.catalog.DeltaCatalog" \
     /home/`whoami`/emr/app.py
